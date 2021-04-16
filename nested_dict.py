ab6da43b368ba5d74fa313e26d9ee253d71059b2
srinath-sankaragallu <61969343+srinath-sankaragallu@users.noreply.github.com>
2021-04-16 15:53:12 +0530

    Update nested_dict.py
    updated the instance type error

--- nested_dict.py
+++ nested_dict.py
@@ -1,21 +1,25 @@
 from functools import reduce
 class dict(dict):
-	def __init__(self,*args,**kwrds):
-		super().__init__(*args,**kwrds)
-	#method 1 for implementing 
-	def nested_keys(self,nested_keys,value):
-		if nested_keys:
-			nested_keys = list(nested_keys)
-			last_key = nested_keys.pop()
-			cur = self
-			for key in nested_keys:
-				cur = cur.setdefault(key,dict())
-			cur[last_key] = value
-	#method 2 for implementing 			
-	def nested_keys1(self,nested_keys,value):
-		if nested_keys:
-			last_key = nested_keys.pop()
-			reduce(lambda m,k: m.setdefault(k,dict()) , nested_keys, self)[last_key] = value
+    def __init__(self,*args,**kwrds):
+        super().__init__(*args,**kwrds)
+    #method implementation one
+    def nested_keys(self,nested_keys, value):
+        if not isinstance(nested_keys, list):
+            raise ValueError("Nested keys must be provided in list not in {} !".format(type(nested_keys)))
+        if nested_keys:
+            nested_keys = list(nested_keys)
+            last_key = nested_keys.pop()
+            cur = self
+            for key in nested_keys:
+                cur = cur.setdefault(key, dict())
+            cur[last_key] = value
+    #method implementation two
+    def nested_keys1(self,nested_keys, value):
+        if not isinstance(nested_keys,list):
+            raise ValueError("Nested keys must be provided in list not in {} !".format(type(nested_keys)))
+        if nested_keys:
+            last_key = nested_keys.pop()
+            reduce(lambda m,k: m.setdefault(k, dict()) , nested_keys, self)[last_key] = value
       
 def main():
   d = dict()
